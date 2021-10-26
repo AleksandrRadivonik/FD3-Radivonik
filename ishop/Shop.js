07eab1381d33425742eb365d3e2ca45d760e4b55
Aleksandr Radivonik <pyuradar@gmail.com>
2021-10-26 12:56:18 +0300

    На проверку. Попытка 1.

--- ishop/Shop.js
+++ ishop/Shop.js
@@ -1,23 +1,36 @@
 var Shop = React.createClass({
 
-    displayName: 'Shop',
+    displayName: "Shop",
   
     getDefaultProps: function() {
         return {name: "Интернет-магазин"}
     },
+
+    propTypes: {
+        name: React.PropTypes.string.isRequired,
+        products: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                article: React.PropTypes.string, 
+                name: React.PropTypes.string, 
+                price: React.PropTypes.number, 
+                photo: React.PropTypes.string, 
+                count: React.PropTypes.number
+            })
+        ),
+    },
   
     render: function() {
         prоductsHead = React.DOM.tr({},
-            React.DOM.td({}, "Наименование"),
-            React.DOM.td({}, "Цена"),
-            React.DOM.td({}, "Фото"),
-            React.DOM.td({}, "Количество"),
+            React.DOM.th({className: "ProductHead"}, "Наименование"),
+            React.DOM.th({className: "ProductHead"}, "Цена"),
+            React.DOM.th({className: "ProductHead"}, "Фото"),
+            React.DOM.th({className: "ProductHead"}, "Количество"),
         );
 
         prоductsTable = this.props.products.map(v => {  
             return React.DOM.tr({key: v.article, className: "Product"},
                 React.DOM.td({className: "ProductName"}, v.name),
-                React.DOM.td({className: "ProductPrice"}, v.price),
+                React.DOM.td({className: "ProductPrice"}, v.price.toFixed(2)),
                 React.DOM.td({className: "ProductPhoto"}, 
                     React.DOM.img({src: v.photo, alt: v.name})
                 ),
