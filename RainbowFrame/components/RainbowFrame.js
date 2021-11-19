03cb686585a29e942850091eeabec2cf67681a65
Aleksandr Radivonik <pyuradar@gmail.com>
2021-11-19 09:41:40 +0300

    RAINBOW_FRAME На проверку

---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,28 @@
+import React, {Fragment} from "react";
+import PropTypes from "prop-types";
+
+class RainbowFrame extends React.Component {
+
+    static propTypes = {
+        colors: PropTypes.arrayOf(PropTypes.string).isRequired
+    };
+
+    render() {
+        let frames =
+            this.props.colors.reduce((elem, color) =>
+                <div style={{border: "solid 5px " + color, padding: "10px", textAlign: "center"}}>
+                    {elem}
+                </div>, // обертка из слоев <div>
+                this.props.children // сначала вложение
+            );
+
+        return (
+            <Fragment>
+                {frames}
+            </Fragment>
+        );
+    }
+
+}
+
+export default RainbowFrame;
